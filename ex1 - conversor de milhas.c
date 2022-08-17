fdee581e9fcbcf1bed11a8392c7349f91bea9efc
Salu Conteratto Ramos <61796219+SaluRamos@users.noreply.github.com>
2022-08-17 13:49:28 -0300

    Update ex1 - conversor de milhas.c

--- ex1 - conversor de milhas.c
+++ ex1 - conversor de milhas.c
@@ -3,6 +3,19 @@
 
 //15 de agosto de 2022
 
+void converter(float milhas, float *km_p, float *mps_p){
+    float c1 = 1.609;
+    float c2 = 3.6;
+    printf("milhas = %f\n", milhas);
+    *km_p = milhas*c1;
+    *mps_p = *km_p/c2;
+}
+
 int main(void){
+    float milhas = 2;
+    float km = 0;
+    float mps = 0;
+    converter(milhas, &km, &mps);
+    printf("km = %f\nmps = %f", km, mps);
     return 1;
 }
