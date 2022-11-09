f6a78dd82be01ae6d6c2a8611663e4e6f0af47f7
Salu Conteratto Ramos <a2150352@vmpdc.utfpr>
2022-11-09 10:46:07 -0300

    appendHashTable, using fnv1a algorithm to append elements in correct index in a array

--- sala de aula/ex12 - hashtable.c
+++ sala de aula/ex12 - hashtable.c
@@ -3,7 +3,8 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdint.h>
-#include "md5/md5.c"
+#include <inttypes.h>
+#include <math.h>
 
 #define FNV_PRIME 1099511628211UL
 
@@ -15,13 +16,22 @@ typedef struct hashobj{
 } HashObject;
 
 typedef struct hashtable{
-    struct hashobj values[100];
-    unsigned long seed;
+    struct hashobj *values;
+    uint64_t seed;
 } HashTable;
 
-HashTable createHashTable(unsigned long seed){
+HashTable createHashTable(uint64_t seed){
     struct hashtable newHashTable;
     newHashTable.seed = seed;
+    newHashTable.values = (struct hashobj *) malloc(seed*sizeof(struct hashobj));
+    for(int i = 0; i < seed; i++){
+        struct hashobj newHashObject;
+        newHashObject.data = -1;
+        newHashObject.hash = NULL;
+        strcpy(newHashObject.key, "");
+        newHashObject.next = NULL;
+        newHashTable.values[i] = newHashObject;
+    }
     return newHashTable;
 }
 
@@ -40,30 +50,41 @@ uint64_t fnv1a(struct hashtable *table, const char* key) {
 
 uint64_t appendHashTable(struct hashtable *table, char *key, int data){
     uint64_t generatedHash = fnv1a(table, key);
-    int appendIndex = generatedHash%table->seed;
-    // struct hashobj newHashObject;
-    // newHashObject.data = data;
-    // newHashObject.hash = generatedHash;
-    // strcpy(newHashObject.key, key);
-    // if(table->values[appendIndex] == NULL){
-    //     table->values[appendIndex] = newHashObject;
-    // }
-    return generatedHash;
+    uint64_t appendIndex = generatedHash%table->seed;
+
+    struct hashobj newHashObject;
+    newHashObject.data = data;
+    newHashObject.hash = generatedHash;
+    strcpy(newHashObject.key, key);
+    newHashObject.next = NULL;
+
+    struct hashobj *appendLocal = table->values[appendIndex];
+
+    while(1){
+        if(appendLocal->next == NULL){
+            appendLocal = newHashObject;
+            break;
+        }else{
+            appendLocal = appendLocal->next;
+        }
+    };
+    
+    return appendIndex;
 }
 
 
 
 int main(){
-    struct hashtable table = createHashTable(16);
-    printf("teste = %d\n", appendHashTable(&table, "a", 100));
-    printf("teste = %d\n", appendHashTable(&table, "b", 100));
-    printf("teste = %d\n", appendHashTable(&table, "c", 100));
-    printf("teste = %d\n", appendHashTable(&table, "d", 100));
-    printf("teste = %d\n", appendHashTable(&table, "e", 100));
-    printf("teste = %d\n", appendHashTable(&table, "bolao", 100));
-    printf("teste = %d\n", appendHashTable(&table, "salu", 100));
-    printf("teste = %d\n", appendHashTable(&table, "bar", 100));
-    printf("teste = %d\n", appendHashTable(&table, "foo", 100));
-    printf("teste = %d\n", appendHashTable(&table, "lokao", 100));
-    print_hash(md5String("teste"));
+    struct hashtable table = createHashTable(200);
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "a", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "b", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "aa", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "bb", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "aaa", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "bbb", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "aaaa", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "bbbb", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "aaaaa", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "testeloko", 100, 0));
+    printf("teste = %" PRIu64 "\n", appendHashTable(&table, "saluu", 100, 0));
 }
