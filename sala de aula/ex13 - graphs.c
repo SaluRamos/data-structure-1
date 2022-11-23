3ce7d0a4bdc88a83a5922afb9b1b8d583841098b
Salu Conteratto Ramos <a2150352@vmpdc.utfpr>
2022-11-23 10:40:25 -0300

    graph allocation, graph free memory, graph addEdge

--- sala de aula/ex13 - graphs.c
+++ sala de aula/ex13 - graphs.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <stdint.h>
+#include <string.h>
 
 
 
 typedef struct{
     int **edges;
+    int dimension;
     int amountEdges;
 } Graph;
-
-
-
-//declaração das funções
-void raiseError(char description[], int statusCode);
 
 
 
@@ -23,18 +21,41 @@ void raiseError(char description[], int statusCode){
 }
 
 
-Graph createGraph(int amountEdges){
+
+Graph createGraph(int dimension){
     Graph newGraph;
-    newGraph.edges = (int *) malloc(pow(amountEdges, 2)*sizeof(int));
+    newGraph.edges = (int **) malloc(dimension*sizeof(int *));
+    for(int i = 0; i < dimension; i++){
+        newGraph.edges[i] = (int *) malloc(dimension*sizeof(int));
+    }
+    newGraph.dimension = dimension;
     newGraph.amountEdges = 0;
     return newGraph;
 
 }
 
 
+
+void addEdge(Graph *g, int vertice_from, int vertice_to, int edge_weight){
+    if(vertice_from > g->dimension || vertice_from < 0 || vertice_to > g->dimension || vertice_to < 0){
+        char errorBuffer[100];
+        sprintf(errorBuffer, "trying to add an edge out of the matrix space, dimension %d, edge %d->%d", g->dimension, vertice_from, vertice_to);
+        raiseError(errorBuffer, 1);
+    }
+    g->amountEdges += 1;
+}
+
+
+
+void freeGraph(Graph *g){
+    free(g->edges);
+}
+
+
+
 int main(void){
 
-    Graph newEdge = createGraph(2);
-    printEdge(&newEdge);
-
+    Graph newGraph = createGraph(2);
+    addEdge(&newGraph, 0, 3, 1);
+    freeGraph(&newGraph);
 }
