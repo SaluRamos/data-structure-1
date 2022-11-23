63cca2b86505c8a2c30189e1757cd82681581e38
Salu Conteratto Ramos <a2150352@vmpdc.utfpr>
2022-11-23 10:56:24 -0300

    graph print

--- sala de aula/ex13 - graphs.c
+++ sala de aula/ex13 - graphs.c
@@ -25,8 +25,13 @@ void raiseError(char description[], int statusCode){
 Graph createGraph(int dimension){
     Graph newGraph;
     newGraph.edges = (int **) malloc(dimension*sizeof(int *));
-    for(int i = 0; i < dimension; i++){
-        newGraph.edges[i] = (int *) malloc(dimension*sizeof(int));
+    for(int row = 0; row < dimension; row++){
+        newGraph.edges[row] = (int *) malloc(dimension*sizeof(int));
+    }
+    for(int row = 0; row < dimension; row++){
+        for(int col = 0; col < dimension; col++){
+            newGraph.edges[row][col] = 0;
+        }
     }
     newGraph.dimension = dimension;
     newGraph.amountEdges = 0;
@@ -42,7 +47,44 @@ void addEdge(Graph *g, int vertice_from, int vertice_to, int edge_weight){
         sprintf(errorBuffer, "trying to add an edge out of the matrix space, dimension %d, edge %d->%d", g->dimension, vertice_from, vertice_to);
         raiseError(errorBuffer, 1);
     }
+    if(edge_weight == 0){
+        edge_weight = 1;
+    }
+    g->edges[vertice_from][vertice_to] = edge_weight;
     g->amountEdges += 1;
+}
+
+
+
+void removeEdge(){
+
+}
+
+
+
+void printGraphMatrix(Graph *g){
+    printf("--------------------------------\n");
+    for(int row = 0; row < g->dimension; row++){
+        for(int col = 0; col < g->dimension; col++){
+            printf(" %d ", g->edges[row][col]);
+        }
+        printf("\n");
+    }
+    printf("--------------------------------");
+}
+
+
+
+void printGraphEdges(Graph *g){
+    printf("--------------------------------\n");
+    for(int row = 0; row < g->dimension; row++){
+        for(int col = 0; col < g->dimension; col++){
+            if(g->edges[row][col] != 0){
+                printf("(%d) %d ---> %d\n", g->edges[row][col], row, col);
+            }
+        }
+    }
+    printf("--------------------------------");
 }
 
 
@@ -56,6 +98,9 @@ void freeGraph(Graph *g){
 int main(void){
 
     Graph newGraph = createGraph(2);
-    addEdge(&newGraph, 0, 3, 1);
+    addEdge(&newGraph, 0, 1, 1);
+    addEdge(&newGraph, 0, 1, 1);
+    printGraphMatrix(&newGraph);
+    printGraphEdges(&newGraph);
     freeGraph(&newGraph);
 }
