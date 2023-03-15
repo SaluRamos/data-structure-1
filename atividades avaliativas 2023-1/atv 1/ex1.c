9a3427383360f4016601d158ce8fe7c17b9544af
Salu Conteratto Ramos <a2150352@vmpdc.utfpr>
2023-03-15 14:34:03 -0300

    atv 1 2023-1 bug fixes

--- atividades avaliativas 2023-1/atv 1/ex1.c
+++ atividades avaliativas 2023-1/atv 1/ex1.c
@@ -4,26 +4,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
-typedef struct{
-    char* state;
-    char* city;
-    char* street;
+typedef struct {
+    char state[20]; //alteração: usar um array de caracteres para guardar o estado
+    char city[30]; //alteração: usar um array de caracteres para guardar a cidade
+    char street[30]; //alteração: usar um array de caracteres para guardar a rua
 } address;
 
-
-
-typedef struct{
-    char* name;
-    address adrs; //endereço, address
-    float salary;
-    char* civilState;
+typedef struct {
+    char name[60]; //alteração: usar um array de caracteres para guardar o nome
+    address adrs;
+    int salary;
+    char civilState[20]; //alteração: usar um array de caracteres para guardar o estado civil
     int age;
-    char* sex; //M ou F
+    char sex[1]; //alteração: usar um array de caracteres para guardar o sexo e definir tamanho 2 para incluir o caractere de terminação de string '\0'
 } employee;
-
-
 
 //abstração para ler próxima linha de um arquivo
 char* readFileLine(FILE *f, int lineMaxSize){
@@ -41,9 +35,7 @@ char* readFileLine(FILE *f, int lineMaxSize){
     return line;
 }
 
-
-
-address createAddress(char* street,char* city, char* state){
+address createAddress(char *street, char *city, char *state) {
     address newAddress;
     strcpy(newAddress.street, street);
     strcpy(newAddress.city, city);
@@ -51,39 +43,32 @@ address createAddress(char* street,char* city, char* state){
     return newAddress;
 }
 
-
-employee createEmployee(char* name, address adrs, float salary, char* civilState, int age, char* sex){
+employee createEmployee(char *name, address adrs, int salary, char *civilState, int age, char *sex) { //alteração: corrigir o tipo do parâmetro salary de float para int
     employee newEmployee;
     strcpy(newEmployee.name, name);
     newEmployee.adrs = adrs;
     newEmployee.salary = salary;
     strcpy(newEmployee.civilState, civilState);
     newEmployee.age = age;
-    newEmployee.sex = sex;
-    // strcpy(newEmployee.sex, sex);
+    strcpy(newEmployee.sex, sex);
     return newEmployee;
 }
 
-void printEmployee(employee slave){
-    //{nome; {rua; cidade; estado} ; salario ; estado civil ; idade; sexo}
-    printf("{%s}; {%s; %s; %s}; %f; %s; %d; %c}\n", slave.name, slave.adrs.street, slave.adrs.city, slave.adrs.state, slave.salary, slave.civilState, slave.age, slave.sex);
+void printEmployee(employee slave) {
+    printf("{%s}; {%s; %s; %s}; %d; %s; %d; %s}\n", slave.name, slave.adrs.street, slave.adrs.city, slave.adrs.state, slave.salary, slave.civilState, slave.age, slave.sex);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     FILE *inputFile;
-    FILE *outputFile; //ordem decrescente
-    if(argc > 1){
+    FILE *outputFile;
+    if (argc > 1) {
         inputFile = fopen(strcat(argv[1], ".txt"), "r");
-    }else{
+    } else {
         inputFile = fopen("funcionarios.txt", "r");
     }
 
-
-
-
-
     char name[60];
-    float salary;
+    int salary;
     char civilState[20];
     int age;
     char sex[1];
@@ -99,10 +84,9 @@ int main(int argc, char *argv[]){
         }
         //{nome; {rua; cidade; estado} ; salario ; estado civil ; idade; sexo}
         sscanf(nextLine, "{%[^;]; {%[^;]; %[^;]; %[^}]}; %[0-9]; %[^;]; %[0-9]; %1[MF]}", name, street, city, state, salary, civilState, age, sex);
-        employeeAddress = createAddress(&street, &city, &state);
-        newEmployee = createEmployee(&name, employeeAddress, salary, &civilState, age, &sex);
+        employeeAddress = createAddress(street, city, state);
+        newEmployee = createEmployee(name, employeeAddress, salary, civilState, age, sex);
         printEmployee(newEmployee);
-        // addNode(newEmployee);
     }
 
 
