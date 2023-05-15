3ecf4b73778b7d42d44ff79bcede4475892a880e
mendeleev <saluc.ramos@outlook.com>
2023-05-15 16:09:31 -0300

    regex implementation, options, comments

--- atividades avaliativas 2023-1/atv 3-4/atv3-4.c
+++ atividades avaliativas 2023-1/atv 3-4/atv3-4.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <math.h>
 #include <stdbool.h>
+#include <regex.h>
 // #include <windows.h>
 #include <ctype.h>
 
@@ -147,6 +148,10 @@ int compareStringAZ(char str1[], char str2[]){
     return 0;
 }
 
+
+
+
+
 client createClient(char* name, char* birthday, char *condition){
     client data;
     //"!\#$%&'()*+,-./0123456789:;<=>?@[\\]^_`{|}~¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ"; caracteres proibidos para nome
@@ -171,7 +176,7 @@ void readRegister(sortedDoublyLinkedList* list){
         addElementToList(list, createClient(name, birthday, condition));
         // printf("adicionando nome %s\n", name);
     }
-    printList(list);
+    // printList(list);
 }
 
 void createRegister(){
@@ -199,9 +204,9 @@ int main(){
     printf("(1) Realizar novo cadastro\n");
     printf("(2) Buscar cadastro\n");
     printf("(3) Alterar dados do cadastro\n");
-    printf("(4) Montar fila de espera\n");
-    printf("(5) Próximo a ir ao buffet\n");
-    printf("(6) Sair do restaurante\n");
+    printf("(4) Montar fila de espera\n"); //realizar a leitura de DadosChegada.txt, verificar quais clientes ir˜ao para fila de espera, quais entrarão imediatamente no restaurante e quais não serão servidos
+    printf("(5) Próximo a ir ao buffet\n"); //tirar uma pessoa da fila de espera e colocá-la na fila do buffet correspondente à sua condição de saúde (se houver espaço no restaurante)
+    printf("(6) Sair do restaurante\n"); //quando um cliente deixa o restaurante, avisar qual cliente está saindo e atualizar a fila repectiva
     printf("(7) Imprimir fila D\n");
     printf("(8) Imprimir fila H\n");
     printf("(9) Imprimir fila N\n");
@@ -209,27 +214,67 @@ int main(){
     printf("(11) Imprimir todas as filas\n");
     printf("(12) Sair\n\n");
 
+    regex_t regex;
+    int reti;
     int selectedOption = 0;
     while(1){
         printf("Digite uma op%c%co: ", 135, 198);
         scanf("%d", &selectedOption);
+        getchar(); //consumir o caractere de nova linha pendente no buffer
+        printf("\n");
+        char name[61] = "";
+        char birthday[11] = "";
+        char condition = 'A';
         switch(selectedOption){
             case 1:
-                break;
+                printf("Digite o nome completo: ");
+                fgets(name, 61, stdin);
+                name[strcspn(name, "\n")] = '\0'; // Remove o \n da string
+                regcomp(&regex, "/^[a-zA-Z ]+$/m", 0);
+                reti = regexec(&regex, name, 0, NULL, 0);
+                printf("resultado regex %d, input = '%s'\n", reti, name);
+                char errbuf[100];
+                regerror(reti, &regex, errbuf, sizeof(errbuf));
+                fprintf(stderr, "Erro ao executar a expressão regular: %s\n", errbuf);
+                if(regexec(&regex, name, 0, NULL, 0) != 0){
+                    printf("vish\n");
+                }
+                regfree(&regex);
+                printf("Digite a data de nascimento: ");
+                fgets(birthday, 11 ,stdin);
+                birthday[strcspn(birthday, "\n")] = '\0'; // Remove o \n da string
+                printf("Digite a condição de saúde: ");
+                scanf(" %c", &condition); //espaço antes de %c para ignorar caracteres em branco
+                printf("voce digitou, %s, %s, %c\n", name, birthday, condition);
                 break;
             case 2:
                 break;
-                break;
             case 3:
-                break;
                 break;
             case 4:
                 break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                free(registers);
+                return 0;
                 break;
             default:
+                printf("comando inválido\n");
                 break;
-
         }
+        printf("\n");
     }
-
 }
